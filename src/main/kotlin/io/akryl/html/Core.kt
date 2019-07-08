107a5ad22b2ad1aab9e5916d65f631568514a4b1
Александр Шилов <aleksandr.schilow2012@gmail.com>
2019-07-08 09:59:48 +0300

    Add all html elements

--- src/main/kotlin/io/akryl/html/Core.kt
+++ src/main/kotlin/io/akryl/html/Core.kt
@@ -1,14 +1,13 @@
 @file:Suppress("FunctionName")
 
-package io.akryl
+package io.akryl.html
 
+import io.akryl.*
 import org.w3c.dom.Element
 import org.w3c.dom.HTMLElement
 import org.w3c.dom.Node
 import org.w3c.dom.css.CSSStyleDeclaration
 import org.w3c.dom.events.Event
-import org.w3c.dom.events.KeyboardEvent
-import org.w3c.dom.events.MouseEvent
 import kotlin.browser.document
 import kotlin.dom.clear
 import kotlin.math.min
@@ -238,155 +237,6 @@ class TextRenderElement(
   }
 }
 
-@Suppress("UNCHECKED_CAST")
-private fun listeners(vararg items: Pair<String, Any?>) = items
-  .filter { it.second != null }
-  .map { Pair(it.first, it.second as (Event) -> Unit) }
-  .toMap()
-
 fun classMap(vararg items: Pair<String, Boolean>) = items
   .filter { it.second }
   .map { it.first }
-
-// todo better types
-class HtmlWidgetFactory(
-  private val tag: String,
-  private val ns: String? = null,
-  private val styled: Styled? = null
-) {
-  operator fun invoke(
-    key: Key? = null,
-    // attributes
-    d: String? = null,
-    disabled: Boolean = false,
-    id: String? = null,
-    `for`: String? = null,
-    height: Int? = null,
-    href: String? = null,
-    placeholder: String? = null,
-    role: String? = null,
-    rows: Int? = null,
-    selected: Boolean = false,
-    src: String? = null,
-    tabIndex: Int? = null,
-    target: String? = null,
-    title: String? = null,
-    type: String? = null,
-    value: String? = null,
-    width: Int? = null,
-    // style
-    clazz: String? = null,
-    classes: List<String?> = emptyList(),
-    style: Map<String, String?> = emptyMap(),
-    // events
-    onBlur: ((event: Event) -> Unit)? = null,
-    onChange: ((event: Event) -> Unit)? = null,
-    onClick: ((event: Event) -> Unit)? = null,
-    onDoubleClick: ((event: Event) -> Unit)? = null,
-    onKeyDown: ((event: KeyboardEvent) -> Unit)? = null,
-    onKeyUp: ((event: KeyboardEvent) -> Unit)? = null,
-    onMouseUp: ((event: MouseEvent) -> Unit)? = null,
-    onMouseDown: ((event: MouseEvent) -> Unit)? = null,
-    onMouseMove: ((event: MouseEvent) -> Unit)? = null,
-    // children
-    child: Widget? = null,
-    children: List<Widget> = emptyList(),
-    innerHtml: String? = null
-  ) = HtmlWidget(
-    tag = tag,
-    ns = ns,
-    cssPrefix = styled?.style()?.prefix,
-    attributes = mapOf(
-      "d" to d,
-      "disabled" to if (disabled) "" else null,
-      "id" to id,
-      "height" to height?.toString(),
-      "for" to `for`,
-      "href" to href,
-      "placeholder" to placeholder,
-      "role" to role,
-      "rows" to rows?.toString(),
-      "selected" to if (selected) "" else null,
-      "src" to src,
-      "tabindex" to tabIndex?.toString(),
-      "target" to target,
-      "title" to title,
-      "type" to type,
-      "value" to value,
-      "width" to width?.toString(),
-      "class" to (classes + clazz)
-        .filterNotNull()
-        .joinToString(" ")
-        .ifEmpty { null }
-    ),
-    style = style,
-    listeners = listeners(
-      "blur" to onBlur,
-      "click" to onClick,
-      "dblclick" to onDoubleClick,
-      "mouseup" to onMouseUp,
-      "mousedown" to onMouseDown,
-      "mousemove" to onMouseMove,
-      "change" to onChange,
-      "keydown" to onKeyDown,
-      "keyup" to { event: KeyboardEvent -> onChange?.invoke(event); onKeyUp?.invoke(event) }
-    ),
-    children = if (child != null) children + child else children,
-    innerHtml = innerHtml,
-    key = key
-  )
-}
-
-val A = HtmlWidgetFactory("a")
-val B = HtmlWidgetFactory("b")
-val Button = HtmlWidgetFactory("button")
-val Div = HtmlWidgetFactory("div")
-val Img = HtmlWidgetFactory("img")
-val Input = HtmlWidgetFactory("input")
-val H1 = HtmlWidgetFactory("h1")
-val H2 = HtmlWidgetFactory("h2")
-val H3 = HtmlWidgetFactory("h3")
-val H4 = HtmlWidgetFactory("h4")
-val H5 = HtmlWidgetFactory("h5")
-val H6 = HtmlWidgetFactory("h6")
-val Label = HtmlWidgetFactory("label")
-val Option = HtmlWidgetFactory("option")
-val Select = HtmlWidgetFactory("select")
-val Span = HtmlWidgetFactory("span")
-val Table get() = HtmlWidgetFactory("table")
-val THead get() = HtmlWidgetFactory("thead")
-val TBody get() = HtmlWidgetFactory("tbody")
-val TD get() = HtmlWidgetFactory("td")
-val TextArea = HtmlWidgetFactory("textarea")
-val TH get() = HtmlWidgetFactory("th")
-val TR get() = HtmlWidgetFactory("tr")
-
-val Path = HtmlWidgetFactory("path", "http://www.w3.org/2000/svg")
-val Svg = HtmlWidgetFactory("svg", "http://www.w3.org/2000/svg")
-
-val Styled.A get() = HtmlWidgetFactory("a", styled = this)
-val Styled.B get() = HtmlWidgetFactory("b", styled = this)
-val Styled.Button get() = HtmlWidgetFactory("button", styled = this)
-val Styled.Div get() = HtmlWidgetFactory("div", styled = this)
-val Styled.Img get() = HtmlWidgetFactory("img", styled = this)
-val Styled.Input get() = HtmlWidgetFactory("input", styled = this)
-val Styled.H1 get() = HtmlWidgetFactory("h1", styled = this)
-val Styled.H2 get() = HtmlWidgetFactory("h2", styled = this)
-val Styled.H3 get() = HtmlWidgetFactory("h3", styled = this)
-val Styled.H4 get() = HtmlWidgetFactory("h4", styled = this)
-val Styled.H5 get() = HtmlWidgetFactory("h5", styled = this)
-val Styled.H6 get() = HtmlWidgetFactory("h6", styled = this)
-val Styled.Label get() = HtmlWidgetFactory("label", styled = this)
-val Styled.Option get() = HtmlWidgetFactory("option", styled = this)
-val Styled.Select get() = HtmlWidgetFactory("select", styled = this)
-val Styled.Span get() = HtmlWidgetFactory("span", styled = this)
-val Styled.Table get() = HtmlWidgetFactory("table", styled = this)
-val Styled.THead get() = HtmlWidgetFactory("thead", styled = this)
-val Styled.TBody get() = HtmlWidgetFactory("tbody", styled = this)
-val Styled.TD get() = HtmlWidgetFactory("td", styled = this)
-val Styled.TextArea get() = HtmlWidgetFactory("textarea", styled = this)
-val Styled.TH get() = HtmlWidgetFactory("th", styled = this)
-val Styled.TR get() = HtmlWidgetFactory("tr", styled = this)
-
-val Styled.Path get() = HtmlWidgetFactory("path", "http://www.w3.org/2000/svg", styled = this)
-val Styled.Svg get() = HtmlWidgetFactory("svg", "http://www.w3.org/2000/svg", styled = this)

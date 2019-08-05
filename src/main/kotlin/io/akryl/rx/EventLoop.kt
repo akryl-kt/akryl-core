386eb22999d2eefc71fdfba096fb82828d469427
Александр Шилов <aleksandr.schilow2012@gmail.com>
2019-08-05 12:13:21 +0300

    Change EventLoop tick from requestAnimationFrame to Promise.then with fallbacks

--- src/main/kotlin/io/akryl/rx/EventLoop.kt
+++ src/main/kotlin/io/akryl/rx/EventLoop.kt
@@ -1,20 +1,34 @@
 package io.akryl.rx
 
+import org.w3c.dom.Window
+import kotlin.browser.window
+import kotlin.js.Promise
+
 object EventLoop {
   private val queue = PriorityQueue<() -> Unit>()
+  private val requestTick = getRequestTick()
 
   fun submit(priority: Int, task: () -> Unit) {
     if (queue.isEmpty) {
-      requestAnimationFrame()
+      requestTick()
     }
     queue.push(priority, task)
   }
 
-  private fun requestAnimationFrame() {
-    val request = js("typeof(window) !== 'undefined' && window.requestAnimationFrame || null")
-    if (request != null) {
-      request(this::drain)
+  private fun getRequestTick(): () -> Unit {
+    if (jsTypeOf(Promise) != "undefined") {
+      return { Promise.resolve(Unit).then { drain() } }
     }
+
+    val global: dynamic = if (jsTypeOf(window) != "undefined") window else global
+
+    val setImmediate = global.setImmediate
+    if (setImmediate != null) {
+      return { setImmediate { drain() }; Unit }
+    }
+
+    val setTimeout = global.setTimeout
+    return { setTimeout({ drain() }, 0); Unit }
   }
 
   fun drain() {
@@ -23,3 +37,5 @@ object EventLoop {
     }
   }
 }
+
+private external val global: Window

933f1619fc02267dc3a04655d27de4df5a694b8a
Александр Шилов <aleksandr.schilow2012@gmail.com>
2020-02-26 22:27:23 +0300

    Add KDoc for Context

--- src/main/kotlin/io/akryl/Context.kt
+++ src/main/kotlin/io/akryl/Context.kt
@@ -5,6 +5,12 @@ import react.ProviderProps
 import react.React
 import react.ReactElement
 
+/**
+ * Accepts a [value] prop to be passed to consuming components that are descendants of this [provider].
+ * One [provider] can be connected to many consumers.
+ * Providers can be nested to override values deeper within the tree.
+ * @see [useContext]
+ */
 fun <T> Context<T>.provider(value: T, children: List<ReactElement<*>>): ReactElement<ProviderProps<T>> {
     return React.createElement(
         Provider,
